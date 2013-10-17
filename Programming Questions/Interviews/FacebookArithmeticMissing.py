b6d32e34ca4c90ce451d956f09fa62295a3c4e85
Abhi Agarwal <me@abhiagarwal.com>
2013-10-17 11:45:52 -0400

    Adding

--- Programming Questions/Interviews/FacebookArithmeticMissing.py
+++ Programming Questions/Interviews/FacebookArithmeticMissing.py
@@ -1,7 +1,6 @@
 # Finds the missing term of arithmetic sequence
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import fileinput
-
 def startfunction():
     # parsing the data
     file_input = fileinput.input()
@@ -27,16 +26,16 @@ def startfunction():
     # we now have the shortest sequence
     # now it is a missing number problem
     thinkLargest = array_values[0] + shortest
-    change = 0
+    change = 0 # will allow us to know if there are more than 1 gaps in the sequence
     for i in range(0, length - 1):
         if (array_values[i] + shortest) != array_values[i + 1]:
             thinkLargest = array_values[i] + shortest
             change += 1
-    
+    # Checks if there is a sequence or not
     if change > 1:
-        print Nil
+        print Nil # Returns if there is no sequence
     else:
-        print thinkLargest
+        print thinkLargest # Returns if there is a sequence
         return
     # it will not display the shortest
   
