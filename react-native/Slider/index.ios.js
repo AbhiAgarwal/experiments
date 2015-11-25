826f28bb8915f466d12bdb24a99453eb06df182e
Abhi Agarwal <me@abhiagarwal.com>
2015-11-25 18:42:03 -0500

    Hadoop/ReactNative/rpc

---
+++ react-native/Slider/index.ios.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var React = require('react-native');
+var { AppRegistry, } = React;
+
+var DrawerLayoutMain = require('./DrawerLayoutMain');
+
+AppRegistry.registerComponent('DrawerLayoutMain', () => DrawerLayoutMain);
